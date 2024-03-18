a70244ce423e79d218c8bbf8b03d042a6ee98b06
takehiakihiro <akihiro2@gmail.com>
2024-03-18 18:45:41 +0900

    公開鍵をJWKにすることとKeyBinding JWTの生成ができた

--- issuer/src/main.rs
+++ issuer/src/main.rs
@@ -3,29 +3,33 @@
 
 use std::error::Error;
 
-use josekit::jws::JwsHeader;
-use josekit::jws::EdDSA;
-use josekit::jwt::JwtPayload;
-use josekit::jwt::{self};
-use sd_jwt_payload::Disclosure;
-use sd_jwt_payload::SdJwt;
-use sd_jwt_payload::SdObjectDecoder;
-use sd_jwt_payload::SdObjectEncoder;
-use sd_jwt_payload::HEADER_TYP;
-use serde_json::json;
-
+use josekit::jws::{JwsHeader, EdDSA};
+use josekit::jwk::alg::ed::EdKeyPair;
+use josekit::jwt::{self, JwtPayload};
+use sd_jwt_payload::{Disclosure, KeyBindingJwtClaims, SdJwt, SdObjectDecoder, SdObjectEncoder, Sha256Hasher, HEADER_TYP};
+use serde_json::{Value, json};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    const PRIVATE_KEY: &str = "issuer_private_key_ed25519.pem";
-    const PUBLIC_KEY: &str = "issuer_public_key_ed25519.pem";
+    const ISSUER_PRIVATE_KEY: &str = "issuer_private_key_ed25519.pem";
+    const ISSUER_PUBLIC_KEY: &str = "issuer_public_key_ed25519.pem";
+    const HOLDER_PRIVATE_KEY: &str = "holder_private_key_ed25519.pem";
 
-    let object = json!({
+    let private_pem_file_content = std::fs::read(HOLDER_PRIVATE_KEY)?;
+    let key_pair = EdKeyPair::from_pem(private_pem_file_content)?;
+    let jwk = key_pair.to_jwk_public_key();
+    let pubkey_jwk = serde_json::from_str(&jwk.to_string())?;
+
+    let mut object = json!({
     "account_name": "example_account",
     "ip_addresses": ["192.168.0.1", "192.168.0.2"],
     "dns_addresses": ["example.com", "example.org"],
     "route_networks": ["10.0.0.0/8", "172.16.0.0/12"],
     "group_name": "example_group"
   });
+
+  if let Value::Object(ref mut map) = object {
+    map.insert("cnf".to_string(), pubkey_jwk);
+  }
 
   let mut encoder: SdObjectEncoder = object.try_into()?;
   let disclosures: Vec<Disclosure> = vec![
@@ -49,7 +53,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
   // Use the encoded object as a payload for the JWT.
   let payload = JwtPayload::from_map(encoder.object()?.clone())?;
-  let private_key = std::fs::read(PRIVATE_KEY).unwrap();
+  let private_key = std::fs::read(ISSUER_PRIVATE_KEY).unwrap();
   let signer = EdDSA.signer_from_pem(private_key)?;
   println!("loaded signer's private key");
   let jwt = jwt::encode_with_signer(&payload, &header, &signer)?;
@@ -59,7 +63,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     .into_iter()
     .map(|disclosure| disclosure.to_string())
     .collect();
-  let sd_jwt: SdJwt = SdJwt::new(jwt, disclosures.clone(), None);
+
+  let nonce = "nonce".to_string();
+  let audience = "el-server".to_string();
+  let hasher = Sha256Hasher::new();
+  let key_binding_jwt = KeyBindingJwtClaims::new(&hasher, jwt.clone(), disclosures.clone(), nonce, audience, 0);
+
+  let holder_private_key = std::fs::read(HOLDER_PRIVATE_KEY).unwrap();
+  let signer = EdDSA.signer_from_pem(holder_private_key)?;
+  println!("loaded signer's private key");
+  let mut header = JwsHeader::new();
+  header.set_algorithm("EdDSA"); // EdDSA署名アルゴリズムの指定
+  header.set_token_type("kb+jwt");
+  let mut payload = JwtPayload::new();
+  payload.set_audience([key_binding_jwt.aud.clone()].to_vec());
+  let now = std::time::SystemTime::now();
+  payload.set_issued_at(&now);
+  payload.set_claim("nonce", Some(Value::String(key_binding_jwt.nonce)))?;
+  payload.set_claim("sd_hash", Some(Value::String(key_binding_jwt.sd_hash)))?;
+  let key_binding_jwt = jwt::encode_with_signer(&payload, &header, &signer)?;
+  println!("kb-jwt: {:?}", key_binding_jwt);
+
+  let sd_jwt: SdJwt = SdJwt::new(jwt, disclosures.clone(), Some(key_binding_jwt));
   let sd_jwt: String = sd_jwt.presentation();
 
   println!("sd_jwt: {:?}", sd_jwt);
@@ -67,7 +92,7 @@ fn main() -> Result<(), Box<dyn Error>> {
   // Decoding the SD-JWT
   // Extract the payload from the JWT of the SD-JWT after verifying the signature.
   let sd_jwt: SdJwt = SdJwt::parse(&sd_jwt)?;
-  let public_key = std::fs::read(PUBLIC_KEY).unwrap();
+  let public_key = std::fs::read(ISSUER_PUBLIC_KEY).unwrap();
   let verifier = EdDSA.verifier_from_pem(public_key)?;
   let (payload, _header) = jwt::decode_with_verifier(&sd_jwt.jwt, &verifier)?;
 
